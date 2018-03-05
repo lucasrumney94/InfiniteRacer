33c86f799e2d0d96fa94b65b53aa25896d050a3e
Lucas Rumney <lucasrumney94@gmail.com>
2018-03-04 19:56:54 -0800

    add basic unity scene

---
+++ InfiniteRacer/Assets/Generate_Straight_Road.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generate_Straight_Road : MonoBehaviour {
+
+	public GameObject roadSegment;
+	public int numberOfRoadSegments = 100;
+	public float roadLength = 10.0f;
+	
+
+	// Use this for initialization
+	void Start () 
+	{
+		for (int i=0; i<numberOfRoadSegments; i++)
+		{
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
