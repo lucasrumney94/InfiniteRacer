ef8efca322802004477b57bfe3af10fe07d39111
Lucas Rumney <lucasrumney94@gmail.com>
2018-03-11 15:53:02 -0700

    add various generation methods

---
+++ InfiniteRacer/Assets/script/GenerateFlyerRoad.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateFlyerRoad : MonoBehaviour {
+
+	public int numberOfRoadSegments = 300;
+	public float roadLength = 1.0f;
+
+	public float AltitudeVariationAmount = 1.0f;
+	public float AltitudeVariationSpeed = .05f;
+	public float TurnVariationAmount = 10.0f;	
+	public float TurnVariationSpeed = 0.01f;
+
+
+	private GameObject RoadSegment;
+
+	void Start () 
+	{
+		RoadSegment = Resources.Load("placeholder/FlyerRoads/RoadSegment") as GameObject;
+
+		for (int i = 0; i<numberOfRoadSegments; i++)
+		{
+			this.transform.position = new Vector3(TurnVariationAmount*Mathf.Sin(TurnVariationSpeed*i),AltitudeVariationAmount*Mathf.Sin(AltitudeVariationSpeed*i),i*roadLength); 
+			GameObject.Instantiate(RoadSegment, this.transform.position, this.transform.rotation);
+		}
+	}
+	
+
+	void Update ()
+	{
+		
+	}
+}
---
+++ InfiniteRacer/Assets/script/GenerateWedgeRoad.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateWedgeRoad : MonoBehaviour {
+
+	private string left = "left";
+	private string right = "right";
+	private string up = "up";
+	private string down = "down";
+	private string straight = "straight";
+
+
+	public Dictionary<string, GameObject> Wedges = new Dictionary<string, GameObject>();
+	public string Direction;
+	
+	public int numberOfRoadSegments = 300;
+	public float roadLength = 1.0f;
+	public float roadHeight = 0.5f;
+	private float currentHeight = 0.0f;
+
+
+	void Start ()
+	{
+		Wedges.Add(straight, Resources.Load("placeholder/wedges/ROAD_S") as GameObject);
+		Wedges.Add(down, Resources.Load("placeholder/wedges/ROAD_SD") as GameObject);
+		Wedges.Add(up, Resources.Load("placeholder/wedges/ROAD_SU") as GameObject);
+
+		Direction = straight;
+		for (int i=0; i<numberOfRoadSegments; i++)
+		{
+			if (i == 20)
+			{
+				Direction = down;
+			}
+
+			if (i == 40)
+			{
+				Direction= straight;
+			}
+
+			if (i == 100)
+			{
+				Direction = up;
+			}
+
+			if (Direction == up)
+			{
+				currentHeight -= roadHeight;
+			}
+			if (Direction == down)
+			{
+				currentHeight += roadHeight;
+			}
+			Instantiate(Wedges[Direction], i*roadLength*Vector3.forward-Vector3.up*currentHeight, Quaternion.identity);
+		}
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+}
