6316e5446f8cd7e4f535e5c954fb287fc19ed5c7
Lucas Rumney <lucasrumney94@gmail.com>
2018-03-14 18:32:05 -0700

    add basic object Pooling
    add basic object Pooling Test

---
+++ InfiniteRacer/Assets/CubeExploder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeExploder : MonoBehaviour {
+
+	ObjectPooler objectPooler;
+
+	void Start()
+	{
+		objectPooler = ObjectPooler.Instance;
+	}
+
+	void FixedUpdate()
+	{
+		objectPooler.SpawnFromPool("cube", transform.position, Quaternion.identity);
+	}
+}
