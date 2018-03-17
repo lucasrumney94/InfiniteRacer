6041f6e3f46e1376773abc7d0e336ae5785871ce
Lucas Rumney <lucasrumney94@gmail.com>
2018-03-16 20:46:52 -0700

    add lane changing
    add test buildings

--- InfiniteRacer/Assets/GenerateQuadFlyerRoad.cs
+++ InfiniteRacer/Assets/GenerateQuadFlyerRoad.cs
@@ -6,6 +6,8 @@ public class GenerateQuadFlyerRoad : MonoBehaviour {
 
 	public GameObject flyerRoadQuad;
 	public float stepDistance = 1.0f;
+	public float laneWidth = 2.5f;
+	public float generationPeriod = 0.5f;
 
 	public float maxTurnTime = 8.0f;
 	public float minTurnTime = 2.0f;
@@ -22,19 +24,31 @@ public class GenerateQuadFlyerRoad : MonoBehaviour {
 	public GameObject RoadLightLeft;
 	public float roadLightPeriod = 5.0f;
 
+	public List<GameObject> buildings;
+	public float buildingPeriod = 5.0f;
+
 	[HideInInspector]
-	public List<Vector3> middleLaneLocations = new List<Vector3>();
+	public Dictionary<string, List<Vector3>> LaneLocations = new Dictionary<string,List<Vector3>>();
 
 	private string[] directions = new string[3];
 	private string direction = "";
 
-	private ObjectPooler objectPooler;
-	
+	[HideInInspector]
+	public ObjectPooler objectPooler;
+
+	private carQuadFlyer playerCar;
+	private int playerCarLastIndex;
+	private bool firstGen = true;
 
 
 	// Use this for initialization
 	void Start () 
 	{
+		LaneLocations["Left"] = new List<Vector3>();
+		LaneLocations["Center"] = new List<Vector3>();
+		LaneLocations["Right"] = new List<Vector3>();
+		
+		playerCar = GameObject.FindGameObjectWithTag("Player").GetComponent<carQuadFlyer>();
 		objectPooler = ObjectPooler.Instance;
 		directions[0] = "straight";
 		directions[1] = "left";
@@ -47,9 +61,10 @@ public class GenerateQuadFlyerRoad : MonoBehaviour {
 		// 	roadQuadPool.Add(g);
 		// }
 
-
 		StartCoroutine(SwitchDirection());
 		StartCoroutine(SpawnRoadLight());
+		StartCoroutine(SpawnTestBuilding());
+		
 	}
 	
 	// Update is called once per frame
@@ -64,13 +79,45 @@ public class GenerateQuadFlyerRoad : MonoBehaviour {
 			transform.Rotate(0,turnSpeed,0);
 		}
 
-		transform.Translate(stepDistance*Vector3.forward, Space.Self);
-		transform.Translate(Vector3.down*0.005f, Space.Self);
+		if (firstGen)
+		{
+			transform.Translate(stepDistance*Vector3.forward, Space.Self);
+			transform.Translate(Vector3.down*0.005f, Space.Self);
 
-		objectPooler.SpawnFromPool("flyerQuad", transform.position, transform.rotation);
-		//Instantiate(flyerRoadQuad, transform.position, transform.rotation);
-		middleLaneLocations.Add(transform.position);
+
+			objectPooler.SpawnFromPool("flyerQuad", transform.position, transform.rotation);
+		
+			//Instantiate(flyerRoadQuad, transform.position, transform.rotation);
+			LaneLocations["Center"].Add(transform.position);
+			LaneLocations["Right"].Add(transform.position+transform.right*laneWidth);
+			LaneLocations["Left"].Add(transform.position-transform.right*laneWidth);
+
+			if (LaneLocations["Center"].Count > objectPooler.poolDictionary["flyerQuad"].Count)
+			{
+				firstGen = false;
+			}
+		}
+		else
+		{
+			if (playerCarLastIndex != playerCar.currentPointIndex)
+			{
+				transform.Translate(stepDistance*Vector3.forward, Space.Self);
+				transform.Translate(Vector3.down*0.005f, Space.Self);
+
+
+				objectPooler.SpawnFromPool("flyerQuad", transform.position, transform.rotation);
+			
+				//Instantiate(flyerRoadQuad, transform.position, transform.rotation);
+				LaneLocations["Center"].Add(transform.position);
+				LaneLocations["Right"].Add(transform.position+transform.right*laneWidth);
+				LaneLocations["Left"].Add(transform.position-transform.right*laneWidth);
+
+				playerCarLastIndex = playerCar.currentPointIndex;
+			}
+		}
+		
 	}
+
 
 	IEnumerator SwitchDirection()
 	{
@@ -108,9 +155,25 @@ public class GenerateQuadFlyerRoad : MonoBehaviour {
 		{
 
 			Instantiate(RoadLightRight, transform.position, transform.rotation);
-			Instantiate(RoadLightLeft, transform.position-5.0f*transform.right, transform.rotation);
+			Instantiate(RoadLightLeft, transform.position-6.0f*transform.right, transform.rotation);
 
 			yield return new WaitForSeconds(roadLightPeriod);
+		}
+	}
+
+	IEnumerator SpawnTestBuilding()
+	{
+		while (true)
+		{
+
+			Instantiate(buildings[Random.Range(0,buildings.Count)], transform.position+8.0f*transform.right, transform.rotation);
+
+			Vector3 myRotation = transform.rotation.eulerAngles;
+			myRotation = new Vector3(myRotation.x, myRotation.y+180, myRotation.z);
+
+			Instantiate(buildings[Random.Range(0,buildings.Count)], transform.position-8.0f*transform.right, Quaternion.Euler(myRotation));
+
+			yield return new WaitForSeconds(buildingPeriod);
 		}
 	}
 }
--- InfiniteRacer/Assets/carQuadFlyer.cs
+++ InfiniteRacer/Assets/carQuadFlyer.cs
@@ -11,17 +11,26 @@ public class carQuadFlyer : MonoBehaviour {
 
 
 	private GenerateQuadFlyerRoad flyer;
-	private int currentPointIndex = 0;
+	//[HideInInspector]
+	public int currentPointIndex = 0;
 
 	private Vector3 newPosition;
 	private Vector3 nextPosition;
 	private Quaternion currentRotation;
 	private float t;
 
+	//public string[] lanes = new string[3];
+	private string[] lanes = { "Left", "Center", "Right"};
+	private int currentLane = 1;
+
 	// Use this for initialization
 	void Start () 
 	{
+		// lanes[0] = "Left";
+		// lanes[1] = "Center";
+		// lanes[2] = "Right";
 		flyer = GameObject.FindGameObjectWithTag("Flyer").GetComponent<GenerateQuadFlyerRoad>();
+
 	}
 	
 	// Update is called once per frame
@@ -35,28 +44,47 @@ public class carQuadFlyer : MonoBehaviour {
 		// 	transform.LookAt(flyer.middleLaneLocations[i+20]);
 		// }
 
-
+		
+		
+		
 		//Smooth Movement
-		t += speed*Time.deltaTime;
-		if (t>=1)
-		{
-			t-=1;
-			currentPointIndex++;
-			currentRotation = transform.rotation;
-		}
+
 		//if (currentPointIndex>flyer.middleLaneLocations.Count-2)
 		//{
 		//	currentPointIndex = 0;
 		//}
 
-		if (flyer.middleLaneLocations.Count > turnLookAhead)
+		if (Input.GetButtonDown("Left"))
 		{
-			newPosition = flyer.middleLaneLocations[currentPointIndex]+height*Vector3.up;
-			nextPosition = flyer.middleLaneLocations[currentPointIndex+1]+height*Vector3.up;
+			if (currentLane > 0)
+			{
+				currentLane -= 1;
+			}
+		}
+		if (Input.GetButtonDown("Right"))
+		{
+			if (currentLane < lanes.Length-1)
+			{
+				currentLane += 1;
+			}
+		}
+
+
+		if (flyer.LaneLocations[lanes[1]].Count > turnLookAhead+2)
+		{
+			t += speed*Time.deltaTime;
+			if (t>=1)
+			{
+				t-=1;
+				currentPointIndex++;
+				currentRotation = transform.rotation;
+			}
+			newPosition = flyer.LaneLocations[lanes[currentLane]][currentPointIndex]+height*Vector3.up;
+			nextPosition = flyer.LaneLocations[lanes[currentLane]][currentPointIndex+1]+height*Vector3.up;
 
 			transform.position = Vector3.Lerp(newPosition, nextPosition, t);
 			
-			transform.rotation = Quaternion.Lerp(currentRotation, Quaternion.LookRotation(flyer.middleLaneLocations[currentPointIndex+turnLookAhead]+height*Vector3.up-newPosition, Vector3.up), t);
+			transform.rotation = Quaternion.Lerp(currentRotation, Quaternion.LookRotation(flyer.LaneLocations[lanes[currentLane]][currentPointIndex+turnLookAhead]+height*Vector3.up-newPosition, Vector3.up), t);
 		}
 
 
