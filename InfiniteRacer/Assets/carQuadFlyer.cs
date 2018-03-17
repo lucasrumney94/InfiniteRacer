ebd055c14b2ff26289e80b3f1cba6e11dde58bff
Lucas Rumney <lucasrumney94@gmail.com>
2018-03-16 18:06:59 -0700

    fix car height

--- InfiniteRacer/Assets/carQuadFlyer.cs
+++ InfiniteRacer/Assets/carQuadFlyer.cs
@@ -6,15 +6,17 @@ public class carQuadFlyer : MonoBehaviour {
 
 	public float speed;
 	public float height;
+	[Range(1,20)]
+	public int turnLookAhead = 10;
 
 
 	private GenerateQuadFlyerRoad flyer;
-	public int currentPointIndex = 0;
+	private int currentPointIndex = 0;
 
 	private Vector3 newPosition;
 	private Vector3 nextPosition;
 	private Quaternion currentRotation;
-	public float t;
+	private float t;
 
 	// Use this for initialization
 	void Start () 
@@ -40,22 +42,23 @@ public class carQuadFlyer : MonoBehaviour {
 		{
 			t-=1;
 			currentPointIndex++;
-			//currentRotation = transform.rotation;
+			currentRotation = transform.rotation;
 		}
 		//if (currentPointIndex>flyer.middleLaneLocations.Count-2)
 		//{
 		//	currentPointIndex = 0;
 		//}
 
-		if (flyer.middleLaneLocations.Count > 5)
+		if (flyer.middleLaneLocations.Count > turnLookAhead)
 		{
 			newPosition = flyer.middleLaneLocations[currentPointIndex]+height*Vector3.up;
 			nextPosition = flyer.middleLaneLocations[currentPointIndex+1]+height*Vector3.up;
 
 			transform.position = Vector3.Lerp(newPosition, nextPosition, t);
+			
+			transform.rotation = Quaternion.Lerp(currentRotation, Quaternion.LookRotation(flyer.middleLaneLocations[currentPointIndex+turnLookAhead]+height*Vector3.up-newPosition, Vector3.up), t);
 		}
 
-		transform.rotation = Quaternion.Lerp(currentRotation, Quaternion.LookRotation(nextPosition-newPosition, Vector3.up), t);
 
 	}
 }
